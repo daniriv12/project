8c1eae86f0e2b549899ef144fe0f1784099500af
Neeena <Stavnn@usi.ch>
2014-05-02 12:31:10 +0200

    Finally working audio recording + fft transform
    yeaaah

---
+++ AudioRecord3/gen/com/example/audiorecord3/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.audiorecord3;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ AudioRecord3/src/com/example/audiorecord3/AudioProcessing.java
@@ -0,0 +1,148 @@
+package com.example.audiorecord3;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.example.audiorecord3.RealDoubleFFT;
+
+public class AudioProcessing extends Activity implements OnClickListener{
+	
+	int frequency = 8000;
+    int channelConfiguration = AudioFormat.CHANNEL_CONFIGURATION_MONO;
+    int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
+    private RealDoubleFFT transformer;
+    int blockSize = 256;
+
+    Button startStopButton;
+    boolean started = false;
+
+    RecordAudio recordTask;
+
+    ImageView imageView;
+    Bitmap bitmap;
+    Canvas canvas;
+    Paint paint;
+
+    //AudioRecord audioRecord;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        startStopButton = (Button) this.findViewById(R.id.StartStopButton);
+        startStopButton.setOnClickListener(this);
+
+        transformer = new RealDoubleFFT(blockSize);
+
+        imageView = (ImageView) this.findViewById(R.id.ImageView01);
+        bitmap = Bitmap.createBitmap((int) 256, (int) 100,
+                Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+        paint = new Paint();
+        paint.setColor(Color.GREEN);
+        imageView.setImageBitmap(bitmap);
+
+    }
+
+    public class RecordAudio extends AsyncTask<Void, double[], Void> {
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+
+            try {
+                // int bufferSize = AudioRecord.getMinBufferSize(frequency,
+                // AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+                int bufferSize = AudioRecord.getMinBufferSize(frequency, 
+                        channelConfiguration, audioEncoding); 
+
+                AudioRecord audioRecord = new AudioRecord( 
+                        MediaRecorder.AudioSource.MIC, frequency, 
+                        channelConfiguration, audioEncoding, bufferSize); 
+
+                short[] buffer = new short[blockSize];
+                double[] toTransform = new double[blockSize];
+
+                audioRecord.startRecording();
+
+                // started = true; hopes this should true before calling
+                // following while loop
+
+                while (started) {
+                    int bufferReadResult = audioRecord.read(buffer, 0,
+                            blockSize);
+
+                    for (int i = 0; i < blockSize && i < bufferReadResult; i++) {
+                        toTransform[i] = (double) buffer[i] / 32768.0; // signed
+                                                                        // 16
+                    }                                       // bit
+                        transformer.ft(toTransform);
+                        Log.e("HZ values", toTransform[0] + " ");
+                        Log.e("HZ values2", 1 * 8000/toTransform[1] + " ");
+                        Log.e("HZ values3", toTransform[2] + " ");
+                        publishProgress(toTransform);
+
+
+
+                }
+
+                audioRecord.stop();
+                audioRecord.release();
+
+            } catch (Throwable t) {
+                t.printStackTrace();
+                Log.e("AudioRecord", "Recording Failed");
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(double[]... toTransform) {
+
+            canvas.drawColor(Color.BLACK);
+
+            for (int i = 0; i < toTransform[0].length; i++) {
+                int x = i;
+                int downy = (int) (100 - (toTransform[0][i] * 10));
+                int upy = 100;
+
+                canvas.drawLine(x, downy, x, upy, paint);
+            }
+
+            imageView.invalidate();
+
+            // TODO Auto-generated method stub
+            // super.onProgressUpdate(values);
+        }
+
+    }
+
+
+    public void onClick(View arg0) {
+        // TODO Auto-generated method stub
+        if (started) {
+            started = false;
+            startStopButton.setText("Start");
+            recordTask.cancel(true);
+        } else {
+            started = true;
+            startStopButton.setText("Stop");
+            recordTask = new RecordAudio();
+            recordTask.execute();
+        }
+    }
+}
