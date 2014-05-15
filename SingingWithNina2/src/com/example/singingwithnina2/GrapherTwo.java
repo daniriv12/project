b6a6ddda3aabaccd56d569550f4b70d3e3744521
Daniel Rivera <riverd@usi.ch>
2014-05-15 13:40:26 +0200

    Latest before hopeful fix with Amanj

--- SingingWithNina2/src/com/example/singingwithnina2/GrapherTwo.java
+++ SingingWithNina2/src/com/example/singingwithnina2/GrapherTwo.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView.GraphViewData;
 import com.jjoe64.graphview.*;
@@ -39,7 +40,13 @@ public class GrapherTwo extends Activity{
 		Bundle extras = intent.getExtras();
 		singData = extras.getIntegerArrayList("sing");
 		melodyData = extras.getIntegerArrayList("melody");
-	
+		
+		
+		
+		ArrayList<Integer>pseudoSing= new ArrayList<Integer>();
+		for (int z = 0 ; z < singData.size(); z++){    //USED ONLY FOR TESTING PURPOSES (SIMULATES RANDOM DATA)
+			pseudoSing.add((250 + (int)(Math.random() * ((450 - 250) + 1))));
+		}
 	
 	
 		
@@ -52,37 +59,41 @@ public class GrapherTwo extends Activity{
 		
 		i = melodyData.size();
 		data = 	new GraphViewData[i];
-		for (int element = 0;element<melodyData.size(); element++){
-			int current = melodyData.get(element);
+		for (int element = 0;element<pseudoSing.size(); element++){ //Replaced melody values for newmelody just for testing, reverse
+			int current = pseudoSing.get(element);
 			data[element] = new GraphViewData(element, current);
 					
 					
 			}
 		data[0] = new GraphViewData(0,0);
 		
-		GraphViewSeriesStyle colour = new GraphViewSeriesStyle();
-		colour.setValueDependentColor(new ValueDependentColor(){
-			public int get(GraphViewDataInterface data) {
-			    // the higher the more red
-			    return Color.RED;
-			  }
-		});
-		GraphViewSeriesStyle colour2 = new GraphViewSeriesStyle();
-		colour2.setValueDependentColor(new ValueDependentColor(){
-			public int get(GraphViewDataInterface data) {
-				    // the higher the more red
-				return Color.BLUE;
-				  }
+		//THIS IS ALL IRRELEVANT IF NOT USING BAR GRAPHS.
+		
+	//	GraphViewSeriesStyle colour = new GraphViewSeriesStyle();
+	//	colour.setValueDependentColor(new ValueDependentColor(){
+	//		public int get(GraphViewDataInterface data) {
+	//		    // the higher the more red
+	//		    return Color.RED;
+	//		  }
+	//	});
+	//	GraphViewSeriesStyle colour2 = new GraphViewSeriesStyle();
+	//	colour2.setValueDependentColor(new ValueDependentColor(){
+	//		public int get(GraphViewDataInterface data) {
+	//			    // the higher the more red
+	//			return Color.BLUE;
+	//			  }
 			
 		
-		});
-		GraphViewSeries melodySeries = new GraphViewSeries("Melody", colour, data);
+	//	});
+		GraphViewSeries singSeries = new GraphViewSeries("PseudoSing", new GraphViewSeriesStyle(Color.WHITE, 1), data);
+		
+		
 		
 		i = singData.size();
 		data = 	new GraphViewData[i];
 		for (int element = 0;element<singData.size(); element++){
 			int current = melodyData.get(element);
-			data[element] = new GraphViewData(element, (current - 100));
+			data[element] = new GraphViewData(element, (current));
 					
 					
 			}
@@ -92,28 +103,42 @@ public class GrapherTwo extends Activity{
 		//by using a for loop quite easily, need further testing
 		
 				
-		GraphViewSeries singSeries = new GraphViewSeries("Voice", colour2, data);
-		
-		GraphView graphView = new BarGraphView(this, "Results");		
+		GraphViewSeries melodySeries = new GraphViewSeries("Melody", new GraphViewSeriesStyle(Color.rgb(255, 153, 0) , 3), data);
+		LineGraphView graphView = new LineGraphView(this, "Results");	
+		graphView.setDrawBackground(false);
 		graphView.addSeries(singSeries);
+		graphView.setDrawBackground(false);
+		//graphView.setBackgroundColor(Color.RED);
+		
 		graphView.getGraphViewStyle().setGridColor(Color.BLACK);
 		graphView.getGraphViewStyle().setNumHorizontalLabels(0);
 		graphView.getGraphViewStyle().setVerticalLabelsColor(Color.WHITE);
 		
 		
-		GraphView graphView2 = new BarGraphView(this, "Results 2");
-		graphView2.addSeries(melodySeries);
+		GraphView graphView2 = new LineGraphView(this, "Results 2");
+		graphView.addSeries(melodySeries);
 		graphView2.getGraphViewStyle().setGridColor(Color.BLACK);
 		graphView2.getGraphViewStyle().setNumHorizontalLabels(0);
 		graphView2.getGraphViewStyle().setVerticalLabelsColor(Color.WHITE);
 		
-		
-		
-		
+		//
 		LinearLayout layout = (LinearLayout) findViewById(R.id.subLayout);
 		layout.addView(graphView);
-		LinearLayout layout2 = (LinearLayout) findViewById(R.id.subLayout2);
-		layout2.addView(graphView2);
+	//	LinearLayout layout2 = (LinearLayout) findViewById(R.id.subLayout);
+		layout.addView(graphView2);
+		
+		TextView scoreSend = (TextView) findViewById(R.id.textScore);
+		Integer score1 = (Integer)score;
+		String score2 = score1.toString();
+		CharSequence score3 = (CharSequence) score2;
+		scoreSend.append(score3);
+		
+		
+		
+		//LinearLayout layout = (LinearLayout) findViewById(R.id.subLayout);
+		//layout.addView(graphView);
+		//LinearLayout layout2 = (LinearLayout) findViewById(R.id.subLayout2);
+		//layout2.addView(graphView2);
 		
 		
 		
